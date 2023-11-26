2e84b5e358e2796fd1e9b611c5fde9ba12e71d22
Adam Narozniak <narozniak.adam4@gmail.com>
2023-11-26 23:53:17 +0100

    Improve naming in emotion recognition

--- 12. Soccer commentary mining/src/emotion_recognition.py
+++ 12. Soccer commentary mining/src/emotion_recognition.py
@@ -77,15 +77,15 @@ def process_audio_file(file_path: str, classifier: Any, batch_duration: int = 12
             end_frames = list(range(start + int(extract_duration * sr), end + int(extract_duration * sr), int(extract_duration * sr)))
             for i, extract in enumerate(extracts):
                 results.append({
-                    "out_prob1": out_prob.numpy()[i][0],
-                    "out_prob2": out_prob.numpy()[i][1],
-                    "out_prob3": out_prob.numpy()[i][2],
-                    "out_prob4": out_prob.numpy()[i][3],
+                    "start_frame": start_frames[i],
+                    "stop_frame": end_frames[i],
+                    "prob_neutral": out_prob.numpy()[i][0],
+                    "prob_angry": out_prob.numpy()[i][1],
+                    "prob_happy": out_prob.numpy()[i][2],
+                    "prob_sad": out_prob.numpy()[i][3],
                     "score": score.numpy()[i],
                     "text_lab": text_lab[i],
                     "index": index.numpy()[i],
-                    "start_frame": start_frames[i],
-                    "stop_frame": end_frames[i]
                 })
 
     return results
@@ -116,11 +116,11 @@ if __name__ == "__main__":
         pymodule_file="custom_interface.py",
         classname="CustomEncoderWav2vec2Classifier"
     )
+    audio_files = find_audio_files('./../data/audio')
     # temporary for a single file load
-    # audio_files = find_audio_files(directory)
-    audio_files = ["./../anger.wav"]
-    audio_files = [
-        "./../data/audio/france_ligue-1/2014-2015/2015-04-05 - 22-00 Marseille 2 - 3 Paris SG/1_224p.mp3"]
+    # audio_files = ["./../anger.wav"]
+    # audio_files = [
+    #     "./../data/audio/france_ligue-1/2014-2015/2015-04-05 - 22-00 Marseille 2 - 3 Paris SG/1_224p.mp3"]
 
     for file in audio_files:
         results = process_audio_file(file, classifier)
